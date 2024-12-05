2a2cdffab86254e8f50b24bb8de423fba442f851
Amitav Roy <reachme@amitavroy.com>
2024-12-05 18:36:59 +0530

    Ignoring vite during these tests

--- tests/Feature/PageResponseTest.php
+++ tests/Feature/PageResponseTest.php
@@ -9,23 +9,28 @@ use function Pest\Laravel\get;
 uses(RefreshDatabase::class);
 
 describe('Testing all page response', function () {
+
     it('loads the login page', function () {
+        $this->withoutVite();
         get(route('login'))->assertOk();
     });
 
     it('loads the home page for auth user', function () {
+        $this->withoutVite();
         actingAs(User::factory()->create());
 
         get(route('home'))->assertOk();
     });
 
     it('loads the product listing page for auth user', function () {
+        $this->withoutVite();
         actingAs(User::factory()->create());
 
         get(route('product.index'))->assertOk();
     });
 
     it('loads the product details page for auth user', function () {
+        $this->withoutVite();
         actingAs(User::factory()->create());
         $product = Product::factory()->create();
 
@@ -33,12 +38,14 @@ describe('Testing all page response', function () {
     });
 
     it('loads the user listing page for auth user', function () {
+        $this->withoutVite();
         actingAs(User::factory()->create());
 
         get(route('user.index'))->assertOk();
     });
 
     it('loads the user details page for auth user', function () {
+        $this->withoutVite();
         $user = User::factory()->create();
 
         actingAs($user);
@@ -47,6 +54,7 @@ describe('Testing all page response', function () {
     });
 
     it('loads the profile page', function () {
+        $this->withoutVite();
         $user = User::factory()->create();
 
         actingAs($user);
