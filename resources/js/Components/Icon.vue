d46f717745925a41028fcc84564baf71caa99ab8
Amitav Roy <reachme@amitavroy.com>
2024-12-06 07:56:40 +0530

    Added the stats card component to dashboard

--- resources/js/Components/Icon.vue
+++ resources/js/Components/Icon.vue
@@ -113,4 +113,18 @@ defineProps({
       d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
     ></path>
   </svg>
+  <svg
+    v-else-if="name === 'up-arrow'"
+    class="w-4 h-4"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
+    <path
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+      d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z"
+    ></path>
+  </svg>
 </template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,6 +2,7 @@
 import PageContainer from '../Components/PageContainer.vue'
 import Breadcrumb from '../Components/Breadcrumb.vue'
 import PageTitle from '../Components/PageTitle.vue'
+import StatsCard from '../Components/StatsCard.vue'
 
 const breadCrumbs = []
 </script>
@@ -12,5 +13,33 @@ const breadCrumbs = []
       <Breadcrumb :links="breadCrumbs" />
       <PageTitle title="Dashboard" />
     </PageContainer>
+
+    <div class="px-4 pt-2">
+      <div
+        class="grid w-full grid-cols-1 gap-4 mt-4 xl:grid-cols-3 2xl:grid-cols-3"
+      >
+        <StatsCard
+          title="New products"
+          stats="2,341"
+          number="12.5%"
+          message="Since last month"
+          icon="layout"
+        />
+        <StatsCard
+          title="New users"
+          stats="2,341"
+          number="11.5%"
+          message="Since last month"
+          icon="users"
+        />
+        <StatsCard
+          title="New customers"
+          stats="2,341"
+          number="15.5%"
+          message="Since last month"
+          icon="users"
+        />
+      </div>
+    </div>
   </div>
 </template>
