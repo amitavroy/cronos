a2e1585598d8166e6f6a8e65ca351cc7d5d73083
Amitav Roy <reachme@amitavroy.com>
2025-01-01 08:54:48 +0530

    Segment listing is done along with tests

---
+++ app/Http/Controllers/SegmentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domain\Segment\Models\Segment;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+
+class SegmentController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function index()
+    {
+        $this->authorize('viewAny', Segment::class);
+
+        $segments = Segment::query()
+            ->orderBy('name')
+            ->paginate(10);
+
+        return inertia('Segment/Index', [
+            'segments' => $segments,
+        ]);
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Segment $segment)
+    {
+        return $segment;
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+}
